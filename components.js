5b8cb1034b3f9960090865102b3d4730839675c1
dctownson <dctownson@gmail.com>
2026-09-14 15:49:30 -0400

    style updates

--- components.js
+++ components.js
@@ -29,7 +29,7 @@ class SiteNav extends HTMLElement {
 
   _themeToggleHTML() {
     return `
-      <button class="theme-toggle" id="themeToggle" data-tip="Toggle theme" aria-label="Toggle theme">
+      <button class="theme-toggle" id="themeToggle" data-tip="Theme" aria-label="Toggle theme">
         <svg class="icon-sun" viewBox="0 0 24 24" width="15" height="15" fill="none" stroke="currentColor" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round">
           <path d="M8 12a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
           <path d="M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7" />
